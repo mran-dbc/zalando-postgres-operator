19d187a93926c0c7f01b7a176f63208282254ca8
Mike Andersen <mran@dbc.dk>
2021-11-02 15:38:52 +0100

    operator-ui

--- manifests/pull-images.sh
+++ manifests/pull-images.sh
@@ -1,18 +1,20 @@
+echo "Pulling images"
 docker pull registry.opensource.zalan.do/acid/logical-backup:v1.7.0
 docker pull registry.opensource.zalan.do/acid/pgbouncer:master-18
 docker pull registry.opensource.zalan.do/acid/postgres-operator:v1.7.0
 docker pull registry.opensource.zalan.do/acid/spilo-14:2.1-p2
+docker pull registry.opensource.zalan.do/acid/postgres-operator-ui:v1.6.0
 
-
+echo "Retagging"
 docker tag registry.opensource.zalan.do/acid/logical-backup:v1.7.0 docker-personal.artifacts.dbccloud.dk/mran/logical-backup:demo
 docker tag registry.opensource.zalan.do/acid/pgbouncer:master-18 docker-personal.artifacts.dbccloud.dk/mran/pgbouncer:demo
 docker tag registry.opensource.zalan.do/acid/postgres-operator:v1.7.0 docker-personal.artifacts.dbccloud.dk/mran/postgres-operator:demo
 docker tag registry.opensource.zalan.do/acid/spilo-14:2.1-p2 docker-personal.artifacts.dbccloud.dk/mran/spilo-14:demo
+docker tag registry.opensource.zalan.do/acid/postgres-operator-ui:v1.6.0 docker-personal.artifacts.dbccloud.dk/mran/postgres-operator-ui:demo
 
-
+echo "Pushing images"
 docker push docker-personal.artifacts.dbccloud.dk/mran/logical-backup:demo
 docker push docker-personal.artifacts.dbccloud.dk/mran/pgbouncer:demo
 docker push docker-personal.artifacts.dbccloud.dk/mran/postgres-operator:demo
 docker push docker-personal.artifacts.dbccloud.dk/mran/spilo-14:demo
-
-
+docker push docker-personal.artifacts.dbccloud.dk/mran/postgres-operator-ui:demo
